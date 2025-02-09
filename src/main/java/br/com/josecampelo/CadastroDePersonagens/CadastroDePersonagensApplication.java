98bd81e4a1a5137f13a7d3af4000e382c134f48f
Campelo <josecampelojr@gmail.com>
2025-02-09 16:54:56 -0300

    Inicializacao do projeto

---
+++ src/main/java/br/com/josecampelo/CadastroDePersonagens/CadastroDePersonagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.josecampelo.CadastroDePersonagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDePersonagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDePersonagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/josecampelo/CadastroDePersonagens/CadastroDePersonagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josecampelo.CadastroDePersonagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePersonagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
