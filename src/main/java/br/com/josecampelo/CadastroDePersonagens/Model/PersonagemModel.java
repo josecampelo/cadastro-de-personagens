254bcd4ffdb7b70df7dd3fb182fc15dfbd76b4b1
Campelo <josecampelojr@gmail.com>
2025-02-09 21:51:38 -0300

    Adiciona as entidades UsuarioModel e PersonagemModel ao projeto

--- src/main/java/br/com/josecampelo/CadastroDePersonagens/Model/PersonagemModel.java
+++ src/main/java/br/com/josecampelo/CadastroDePersonagens/Model/PersonagemModel.java
@@ -1,4 +1,23 @@
 package br.com.josecampelo.CadastroDePersonagens.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
 public class PersonagemModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String fonte;
+    private String imagem;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private UsuarioModel usuario;
 }
--- src/main/java/br/com/josecampelo/CadastroDePersonagens/Model/UsuarioModel.java
+++ src/main/java/br/com/josecampelo/CadastroDePersonagens/Model/UsuarioModel.java
@@ -1,4 +1,22 @@
 package br.com.josecampelo.CadastroDePersonagens.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
 public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PersonagemModel> personagensFavoritos;
 }
