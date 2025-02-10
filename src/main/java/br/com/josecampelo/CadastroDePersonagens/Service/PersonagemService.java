744c6bdb19365ed2ad9b00e9f18ee9f88791f37b
Campelo <josecampelojr@gmail.com>
2025-02-09 22:01:25 -0300

    Adiciona a service UsuarioService ao projeto

--- src/main/java/br/com/josecampelo/CadastroDePersonagens/Service/PersonagemService.java
+++ src/main/java/br/com/josecampelo/CadastroDePersonagens/Service/PersonagemService.java
@@ -1,4 +1,8 @@
 package br.com.josecampelo.CadastroDePersonagens.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonagemService {
+
 }
--- src/main/java/br/com/josecampelo/CadastroDePersonagens/Service/UsuarioService.java
+++ src/main/java/br/com/josecampelo/CadastroDePersonagens/Service/UsuarioService.java
@@ -1,4 +1,24 @@
 package br.com.josecampelo.CadastroDePersonagens.Service;
 
+import br.com.josecampelo.CadastroDePersonagens.Model.UsuarioModel;
+import br.com.josecampelo.CadastroDePersonagens.Repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public UsuarioModel adicionaUsuario(UsuarioModel usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public List<UsuarioModel> listarUsuarios(){
+        return usuarioRepository.findAll();
+    }
 }
